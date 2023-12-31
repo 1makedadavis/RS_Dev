86b57eee5647b2c1f37d9878bcb7c42d3bdc1373
Makeda Davis <1makedadavis@gmail.com>
2023-12-30 21:00:02 -0500

    Change font

--- example/components/Example.jsx
+++ example/components/Example.jsx
@@ -7,7 +7,7 @@ const otherFontTheme = {
   fontFamily: 'Helvetica Neue',
   headerBgColor: '#6e48aa',
   headerFontColor: '#fff',
-  headerFontSize: '16px',
+  headerFontSize: '17px',
   botBubbleColor: '#6E48AA',
   botFontColor: '#fff',
   userBubbleColor: '#fff',
